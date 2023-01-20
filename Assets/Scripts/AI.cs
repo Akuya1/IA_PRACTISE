0898b72d593a0f91279465af26f567aa90298c59
Akuya <aiyuegonzalbes@gmail.com>
2023-01-20 09:48:47 +0100

    AI

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    enum State
+    {
+        Patrolling,
+        Chasing
+    }
+
+    State currentState;
+    public Transform[] destinationPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case State.Patrolling:
+                Patrol();
+            break;
+            case State.Chasing:
+                Chase();
+            break;
+            default:
+                Chase();
+            break;
+        }
+    }
+
+    void Patrol()
+    {
+
+    }
+
+    void Chase()
+    {
+
+    }
+
+    void OnDrawGizmos() 
+    {
+        foreach (Transform point in destinationPoints)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(point.position, 1);
+        }
+    }
+}
